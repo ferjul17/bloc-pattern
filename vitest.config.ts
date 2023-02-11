6c73afa9507cc2e111fd42dce5d9770bf350f75c
Julien F <github@ferrier.email>
2023-02-11 22:34:26 +0100

    chore: add github action

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 });
